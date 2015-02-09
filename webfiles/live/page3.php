3a9db719e86cb12da91d81b35e66c4ba3b84dbe3
Scott Herbert <Scott.a.herbert@googlemail.com>
2015-02-09 23:50:18 +0000

    Added very basic related meta search
    Signed-off-by: Scott Herbert <Scott.a.herbert@googlemail.com>

--- webfiles/live/page3.php
+++ webfiles/live/page3.php
@@ -1,6 +1,31 @@
-	
+<?php
 
-    <!doctype html>
+include_once('inc\settings.php');
+
+$clean_Meta = (int)$_GET['id'];
+
+$SQL = "SELECT i.Title, i.Description, i.id, i.JSON FROM item AS i WHERE i.id = " . $clean_Meta;
+
+$Title = "";
+$Desc = "";
+$ID = "";
+$JSON = "";
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=meta', $DB_USER, $DB_PASS);
+    foreach($dbh->query($SQL) as $row) {
+	$Title = $row['Title'];
+	$Desc = $row['Description'];
+	$id = $row['id'];
+	$JSON = $row['JSON'];
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?><!doctype html>
     <html>
     <head>
     <meta  http-equiv="Content-Type" content="text/html; charset="utf-8">
@@ -16,21 +41,40 @@
      
     <body>
      
-    <h1>Title</h1>
-    <h2>&ensp;&ensp;This is a description + non-</br>visual data from JSON</h2>
+    <h1><?php echo $title; ?></h1>
+    <h2>&ensp;&ensp;<?php echo $Desc; ?></br><?php echo $JSON; ?></h2>
 
     <div id="map">
      <img src="http://placehold.it/300x200" />
     </div>
 	<div id="clear"></div>
     </hr>
-    <h1><a href="page3.html">Search Result 1</a></h1>
-    <h2>&ensp;&ensp;This is a description</h2>
-    &ensp;
-    <h1><a href="page3.html">Search Result 2</a></h1>
-    <h2>&ensp;&ensp;This is a description</h2>
-    &ensp;
-     
+    <h2>The following other search terms may be of intrest to you</h2>
+<?php
+
+// to do add a meta search engion here
+//
+// Five minite job....
+
+
+// use id to get a list of meta id's
+
+$SQL = "SELECT meta_desc from meta_link as ml join meta as m on ml.meta_id = m.meta_id where ml.item_id = " . $clean_Meta;
+
+// List them here (with a link to page 2)
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=meta', $DB_USER, $DB_PASS);
+    foreach($dbh->query($SQL) as $row) {
+	echo "<h3><a href='page2.php?inputBox=".$row['meta_desc']."'>".$row['meta_desc']."</a></h3>";
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+?>
     </body>
     </html>
 
